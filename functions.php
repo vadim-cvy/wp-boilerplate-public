09aeeb8b5e39791cb3618ac9461e59834aca0405
Vadim Cherepenichev <vadim.cvy@gmail.com>
2024-09-25 02:16:43 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once __DIR__ . '/cvy-init.php';
+
+// todo: init your classes from `/inc/` here:
+// Cvy\ClassDirName\ClassName::get_instance();
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// todo: require_once parent theme index file here if you're building a child theme
