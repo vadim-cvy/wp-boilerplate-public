731bca35d245e0b0bc8c39f49e6e1831e8953cb6
Vadim Cherepenichev <vadim.cvy@gmail.com>
2024-09-25 23:08:25 +0300

    Implement boilerplate init

---
+++ boilerplate-init.php
@@ -0,0 +1,25 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'YOURAPP_ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR );
+define( 'YOURAPP_TEMPLATES_DIR', YOURAPP_ROOT_DIR . 'template-parts' . DIRECTORY_SEPARATOR );
+define( 'YOURAPP_ASSETS_DIST_DIR', YOURAPP_ROOT_DIR . 'assets/dist' . DIRECTORY_SEPARATOR );
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+YourApp\SiteSetupValidator\SiteSetupValidator::get_instance();
+
+// todo: remove this line and corresponding dir if you don't use All-in-One WP Migration
+YourApp\ThirdpartyPlugins\AI1M\AI1M::get_instance();
+
+// todo: remove this line and corresponding dir if you don't use Beaver Builder
+YourApp\ThirdpartyPlugins\BeaverBuilder\BeaverBuilder::get_instance();
+
+// todo: remove this line and corresponding dir if you don't use Beaver Builder Themer
+YourApp\ThirdpartyPlugins\BeaverBuilderThemer\BeaverBuilderThemer::get_instance();
+
+if ( ! is_admin() )
+{
+  // todo: remove astra dependency if not needed
+  \YourApp\Utils\Assets\Assets::enqueue_local_css( 'global', [ 'astra-theme-css' ] );
+}
