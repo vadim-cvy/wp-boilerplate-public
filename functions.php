15f1634442da238aae5bc71e207e8ab91399641f
Vadim Cherepenichev <vadim.cvy@gmail.com>
2024-09-25 23:39:04 +0300

    Prepare theme unique files (prefixed)

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-require_once __DIR__ . '/boilerplate-init.php';
-
-// todo: init your classes from `/inc/` here:
-// YourApp\ClassDirName\ClassName::get_instance();
---
+++ theme.functions.php
@@ -0,0 +1,6 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once __DIR__ . '/boilerplate-init.php';
+
+// todo: read "README.md >> PHP Development" before you start PHP coding
