ecd7c208dd5e258b0a43b6d35b3cb6d82d6e2d8d
Vadim Cherepenichev <vadim.cvy@gmail.com>
2024-09-25 23:39:17 +0300

    Prepare plugin unique files (prefixed)

---
+++ plugin.your-plugin-name.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * todo: set the plugin name
+ * Plugin Name: My Plugin
+ * Version: 1.0.0
+ * Text Domain: yourapp
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once __DIR__ . '/boilerplate-init.php';
+
+// todo: read "README.md >> PHP Development" before you start PHP coding
